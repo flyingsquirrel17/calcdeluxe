e15838f7b72b36bce39a71d82f8e3ace873c0d9a
daniel <daniel@Sim-PC.fritz.box>
2017-04-05 15:04:00 +0200

    jetzt mit javafx

--- src/calcdeluxe2/Hauptklasse.java
+++ src/calcdeluxe2/Hauptklasse.java
@@ -1,12 +1,34 @@
 package calcdeluxe2;
 
-public class Hauptklasse {
+import javafx.application.*;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hallo Welt");
-		System.out.println("zurück");
 
-	}
 
+public class Hauptklasse extends Application {
+    private Label label;
+
+    @Override public void init() {
+        label = new Label("Hello World");
+    }
+
+    @Override public void start(Stage stage) {
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 200, 200);
+
+        stage.setTitle("Hello World Example");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override public void stop() {}
+
+    public static void main(String[] parameters) {
+        launch(parameters);
+    }
 }
