0523c78a2220612093cde44f4ed4fb611fa0e06a
daniel <daniel@Sim-PC.fritz.box>
2017-04-05 14:45:51 +0200

    test

---
+++ src/calcdeluxe2/Hauptklasse.java
@@ -0,0 +1,11 @@
+package calcdeluxe2;
+
+public class Hauptklasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo Welt");
+
+	}
+
+}
